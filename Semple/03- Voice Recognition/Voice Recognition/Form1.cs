f742f9aa564b889b889ee7e2d5abce00ec514d97
Dushyant Singh <dushyantsingh.dsindia@gmail.com>
2021-11-23 00:24:21 +0530

    Voice recog.

---
+++ Semple/03- Voice Recognition/Voice Recognition/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Speech;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Voice_Recognition
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer ss = new SpeechSynthesizer();
+        PromptBuilder pb = new PromptBuilder();
+        SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+        Choices clist;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_start_Click(object sender, EventArgs e)
+        {
+            btn_start.Enabled = false;
+            btn_stop.Enabled = true;
+
+            clist.Add(new string[] { "hello", "how are you", "open google", "thank you", "close" });
+
+            Grammar gr = new Grammar(new GrammarBuilder(clist));
+
+            try
+            {
+                sre.RequestRecognizerUpdate();
+                sre.LoadGrammar(gr);
+                sre.SpeechRecognized += sre_SpeechRecognized;
+                sre.SetInputToDefaultAudioDevice();
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text.ToString())
+            {
+                case "hello":
+                    ss.SpeakAsync("hello Sekhar");
+                    break;
+                case "how are you":
+                    ss.SpeakAsync("iam doing great sekhar how about you");
+                    break;
+                case "what is the current time":
+                    break;
+                case "Open chrome":
+                    
+                    Process.Start("chrome", "https:google.com");
+                    break;
+
+            }
+        }
+    }
+}
