db3dbe7afca69207cbb42e4e8c690f8b54fca040
Dushyant Singh <55306442+Dushyantsingh-ds@users.noreply.github.com>
2022-03-04 14:55:19 -0800

    Add files via upload

---
+++ Semple/10- File Upload and DOwnload web api .Net Core/Controllers/TestFileController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Threading;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace FileUploadDownload.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TestFileController : ControllerBase
+    {
+
+        private async Task<string> WriteFile(IFormFile file)
+        {
+            string fileName = "";
+            try
+            {
+                var extension = "." + file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
+                fileName = DateTime.Now.Ticks + extension;
+                var pathBuilt = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files");
+                if (!Directory.Exists(pathBuilt))
+                {
+                    Directory.CreateDirectory(pathBuilt);
+                }
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files",
+                   fileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+            catch (Exception e)
+            {
+            }
+            return fileName;
+        }
+
+
+        [HttpPost]
+        [Route("SaveFile")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UploadFile(IFormFile file, CancellationToken cancellationToken)
+        {
+
+            var result = await WriteFile(file);
+
+
+            return Ok(result);
+        }
+
+
+
+        [HttpGet("DownloadFile")]
+        public async Task<ActionResult> DownloadFile(string NameFile)
+        {
+            // ... code for validation and get the file
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files",
+                   NameFile);
+            var provider = new FileExtensionContentTypeProvider();
+            if (!provider.TryGetContentType(filePath, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = await System.IO.File.ReadAllBytesAsync(filePath);
+            return File(bytes, contentType, Path.GetFileName(filePath));
+        }
+    }
+}
