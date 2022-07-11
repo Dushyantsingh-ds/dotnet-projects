dec1e39bd67b21a0317f20dc98daa92a833210f0
Dushyant Singh <dushyantsingh.dsindia@gmail.com>
2022-07-11 18:32:49 +0530

    Partial view

---
+++ Semple/12- PartialView Layout MVC/PartialView/Controllers/PartialController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialView.Controllers
+{
+    public class PartialController : Controller
+    {
+        // GET: Partial
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult view1()
+        {
+            return PartialView();
+        }
+        public PartialViewResult view2()
+        {
+            return PartialView();
+        }
+    }
+}
